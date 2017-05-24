f640963bfb1817da043a1c83cd75c45bfde4f856
Brenda <brenda.o.ramires@gmail.com>
2017-05-24 19:02:14 -0300

    App.js updated with POST and GET for notas

--- app.js
+++ app.js
@@ -156,32 +156,25 @@ router.route('/alunos/:ra')   // operacoes sobre um aluno (RA)
     }
   )
   .delete(function(req, res) {   // DELETE (remove)
-     var response = {};
-     var query = {"ra": req.params.ra};
-      mongoOp.findOneAndRemove(query, function(erro, data) {
-         if(erro) {
-            response = {"resultado": "falha de acesso ao DB"};
-            res.json(response);
-	 } else if (data == null) {	      
-             response = {"resultado": "aluno inexistente"};
-             res.json(response);
+  		var response = {};
+    	var query = {"ra": req.params.ra};
+     	mongoOp.findOneAndRemove(query, function(erro, data) {
+        	if(erro) {
+            	response = {"resultado": "falha de acesso ao DB"};
+            	res.json(response);
+	 		} else if (data == null) {	      
+            	response = {"resultado": "aluno inexistente"};
+            	res.json(response);
             } else {
-              response = {"resultado": "aluno removido do BD"};
-              res.json(response);
-
-	   }
-         }
-       )
-     }
-  );
+            	response = {"resultado": "aluno removido do BD"};
+            	res.json(response);
+	   		}
+        })
+   });
 
 router.route('/filmes')   // operacoes sobre todos os filmes
   .get(function(req, res) {  // GET
 
-	//console.log(req.path); 
-	//console.log(JSON.stringify(req.body));	
-	//res.status(200).send('String test');
-	
     var response = {};
     filmeOp.find({}, function(erro, data) {
        if(erro)
@@ -194,150 +187,150 @@ router.route('/filmes')   // operacoes sobre todos os filmes
     }
   )
   .post(function(req, res) {   // POST (cria)
-	//console.log(req.path); 
-	//console.log(JSON.stringify(req.body));	
-	//res.status(200).send('String test');
-	//RESTO NAO NECESSARIO PARA AGORA, SOH CRIAR OS TEMPLATES	 
-     var query = {"id": req.body.id};
-     var response = {};
-     filmeOp.findOne(query, function(erro, data) {
-        if (data == null) {
-           var db = new filmeOp(); //TODO checar se algum campo obrigatorio esta vazio...
-           db.id = req.body.id;
-           db.titulo = req.body.titulo;
-	   db.elenco = req.body.elenco;
-           db.diretor = req.body.diretor;
-	  // db.dt = req.body.diretor;
-//	"dataLanc": Date,
-//	"rank": Number,
-//	"generos": [String],
-//	"trailer": String
-           db.save(function(erro) {
-             if(erro) {
-                 response = {"resultado": "Falha de insercao no BD"};
-                 res.json(response);
-             } else {
-                 response = {"resultado": "Filme inserido no BD"};
-                 res.json(response);
-              }
+
+     	var query = {"id": req.body.id};
+     	var response = {};
+     	filmeOp.findOne(query, function(erro, data) {
+        	if (data == null) {
+        		var db = new filmeOp(); //TODO checar se algum campo obrigatorio esta vazio...
+           		db.id = req.body.id;
+           		db.titulo = req.body.titulo;
+	   			db.elenco = req.body.elenco;
+           		db.diretor = req.body.diretor;
+           		db.save(function(erro) {
+             		if(erro) {
+                		response = {"resultado": "Falha de insercao no BD"};
+                 		res.json(response);
+             		} else {
+                    	response = {"resultado": "Filme inserido no BD"};
+                		res.json(response);
+              		}
+           		})
+        	} else {
+	    		response = {"resultado": "Filme ja existente"};
+            	res.json(response);
             }
-          )
-        } else {
-	    response = {"resultado": "Filme ja existente"};
-            res.json(response);
-          }
-        }
-      )
-    }
-  )
+        })
+  })
   .delete(function(req, res) {	
 		console.log(req.path); 
 		console.log(JSON.stringify(req.body));
 		res.status(200).send('String test');
-  	}
-  );
+  });
+
 
 router.route('/filmes/:id')   // operacoes sobre um filme(id)
   .get(function(req, res) {   // GET
-     		console.log(req.path); 
+     	console.log(req.path); 
 		console.log(JSON.stringify(req.body));
 		res.status(200).send('String test');
     }
   )
   .put(function(req, res) {   // PUT (altera)
-     		console.log(req.path); 
+     	console.log(req.path); 
 		console.log(JSON.stringify(req.body));
 		res.status(200).send('String test');
-    }
-  )
+  })
   .delete(function(req, res) {   // DELETE (remove)
-    		console.log(req.path); 
+    	console.log(req.path); 
 		console.log(JSON.stringify(req.body));
 		res.status(200).send('String test');
-     }
-  );
+  });
 
 
 router.route('/notas')   // operacoes sobre todas as notas
   .get(function(req, res) {  // GET
-	//console.log(req.path); 
-	//console.log(JSON.stringify(req.body));	
-	//res.status(200).send('String test');
 
 	var response = {};
 	notaOp.find({}, function(erro, data) {
-        if(erro)
-          response = {"resultado": "Falha de acesso ao BD"};
-        else
-          response = {"notas": data};
-          res.json(response);
+    	
+    	if(erro) {
+        	response = {"resultado": "/notas: Falha de acesso ao BD"};
+        } else {
+        	response = {"notas": data};
+        	res.json(response);
         }
-      )
-
-    }
-  )
+        
+    })
+  })
   .post(function(req, res) {   // POST (cria)
-	console.log(req.path); 
-	console.log(JSON.stringify(req.body));	
-	res.status(200).send('String test');
-    }
-  )
+	
+	var query = {"id": req.body.id};
+    var response = {};
+    notaOp.findOne(query, function(erro, data) {
+    	
+    	if (data == null) {
+    	
+        	var db = new notaOp(); //TODO checar se algum campo obrigatorio esta vazio...
+        	db.id = req.body.id;
+        	db.valor = req.body.valor;
+        	db.filmeId = req.body.filmeId;
+        	db.usuarioId = req.body.usuarioId;
+        	db.save(function(erro) {
+            	if(erro) {
+            		response = {"resultado": "notas: Falha de insercao no BD"};
+                	res.json(response);
+             	} else {
+                	response = {"resultado": "Nota inserida no BD"};
+                	res.json(response);
+             	}
+            })
+            
+        } else {
+	    	response = {"resultado": "Nota ja existente"};
+            res.json(response);
+        }
+	})
+
+  })
   .delete(function(req, res) {	
 		console.log(req.path); 
 		console.log(JSON.stringify(req.body));
 		res.status(200).send('String test');
-  	}
-  );
+  });
+
 
 router.route('/notas/:id')   // operacoes sobre uma nota(id)
   .put(function(req, res) {   // PUT (altera)
-     		console.log(req.path); 
+  		console.log(req.path); 
 		console.log(JSON.stringify(req.body));
 		res.status(200).send('String test');
-    }
-  );
+    });
 
 
 router.route('/usuarios')   // operacoes sobre todos os usuarios
   .get(function(req, res) {  // GET
-	console.log(req.path); 
-	console.log(JSON.stringify(req.body));	
-	res.status(200).send('String test');
-
-    }
-  )
+		console.log(req.path); 
+		console.log(JSON.stringify(req.body));	
+		res.status(200).send('String test');
+  })
   .post(function(req, res) {   // POST (cria)
-	console.log(req.path); 
-	console.log(JSON.stringify(req.body));	
-	res.status(200).send('String test');
-    }
-  )
+		console.log(req.path); 
+		console.log(JSON.stringify(req.body));	
+		res.status(200).send('String test');
+  })
   .delete(function(req, res) {	
 		console.log(req.path); 
 		console.log(JSON.stringify(req.body));
 		res.status(200).send('String test');
-  	}
-  );
+  });
 
 router.route('/usuarios/:id')   // operacoes sobre um usuario(id)
   .get(function(req, res) {   // GET
-     		console.log(req.path); 
+     	console.log(req.path); 
 		console.log(JSON.stringify(req.body));
 		res.status(200).send('String test');
-    }
-  )
+  })
   .put(function(req, res) {   // PUT (altera)
-     		console.log(req.path); 
+     	console.log(req.path); 
 		console.log(JSON.stringify(req.body));
 		res.status(200).send('String test');
-    }
-  )
+  })
   .delete(function(req, res) {   // DELETE (remove)
-    		console.log(req.path); 
+    	console.log(req.path); 
 		console.log(JSON.stringify(req.body));
 		res.status(200).send('String test');
-     }
-  );
+  });
 
 router.route('/listas')   // operacoes sobre todas as listas
   .get(function(req, res) {  // GET
@@ -362,21 +355,18 @@ router.route('/listas')   // operacoes sobre todas as listas
 
 router.route('/listas/:id')   // operacoes sobre uma lista(id)
   .get(function(req, res) {   // GET
-     		console.log(req.path); 
+     	console.log(req.path); 
 		console.log(JSON.stringify(req.body));
 		res.status(200).send('String test');
-    }
-  )
+  })
   .put(function(req, res) {   // PUT (altera)
-     		console.log(req.path); 
+     	console.log(req.path); 
 		console.log(JSON.stringify(req.body));
 		res.status(200).send('String test');
-    }
-  )
+  })
   .delete(function(req, res) {   // DELETE (remove)
-    		console.log(req.path); 
+    	console.log(req.path); 
 		console.log(JSON.stringify(req.body));
 		res.status(200).send('String test');
-     }
-  );
+  });
 
